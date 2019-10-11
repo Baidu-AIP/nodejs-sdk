261705ceb18f7ad2079dd6b800f997c2309d3243
王志超 <1696250989@qq.com>
2019-10-11 09:41:23 +0800

    添加人脸搜搜m:n

--- src/AipFace.d.ts
+++ src/AipFace.d.ts
@@ -55,6 +55,22 @@ declare class AipFace extends BaseClient {
      * @return {Promise} - 标准Promise对象
      */
     search(image: any, imageType: any, groupIdList: any, options: any): Promise<AipFace.IAipFaceSearchReturn>;
+    	/**
+     * 人脸搜索 M:N 识别接口
+     *
+     * @param {string} image - 图片信息(**总数据大小应小于10M**)，图片上传方式根据image_type来判断
+     * @param {string} imageType - 图片类型 **BASE64**:图片的base64值，base64编码后的图片数据，需urlencode，编码后的图片大小不超过2M；**URL**:图片的 URL地址( 可能由于网络等原因导致下载图片时间过长)**；FACE_TOKEN**: 人脸图片的唯一标识，调用人脸检测接口时，会为每个人脸图片赋予一个唯一的FACE_TOKEN，同一张图片多次检测得到的FACE_TOKEN是同一个
+     * @param {string} groupIdList - 从指定的group中进行查找 用逗号分隔，**上限20个**
+     * @param {Object} options - 可选参数对象，key: value都为string类型
+     * @description options - options列表:
+     *   max_face_num 最多处理人脸的数目<br>**默认值为1(仅检测图片中面积最大的那个人脸)** **最大值10**
+     *   match_threshold 匹配阈值（设置阈值后，score低于此阈值的用户信息将不会返回） 最大100 最小0 默认80 <br>**此阈值设置得越高，检索速度将会越快，推荐使用默认阈值`80`**
+     *   quality_control 图片质量控制  **NONE**: 不进行控制 **LOW**:较低的质量要求 **NORMAL**: 一般的质量要求 **HIGH**: 较高的质量要求 **默认 NONE**
+     *   liveness_control 活体检测控制  **NONE**: 不进行控制 **LOW**:较低的活体要求(高通过率 低攻击拒绝率) **NORMAL**: 一般的活体要求(平衡的攻击拒绝率, 通过率) **HIGH**: 较高的活体要求(高攻击拒绝率 低通过率) **默认NONE**
+     *   max_user_num 查找后返回的用户数量。返回相似度最高的几个用户，默认为1，最多返回50个。
+     * @return {Promise} - 标准Promise对象
+     */
+	multiSearch(image: any, imageType: any, groupIdList: any, options: any): Promise<AipFace.IAipFaceMNReturn>;
     /**
      * 人脸注册接口
      *
@@ -313,6 +329,24 @@ export declare namespace AipFace {
         [k: string]: any;
     }
     //人脸注册检测结构-end
+    //人脸搜索M:N结构-start
+	interface IAipFaceMNReturn {
+		error_msg: any;
+		error_code?: any;
+		timestamp: any;
+		result?: any | IAipFaceMNResult;
+		log_id: number;
+	}
+	interface IAipFaceMNResult {
+		face_num: string;
+		face_list: IAipFaceMNFaceList[];
+	}
+	interface IAipFaceMNFaceList {
+		face_token: string;
+		location: any;
+		user_list: IAipFaceSearchUserInfo[];
+	}
+	//人脸搜索M:N结构-end
 }
 export default AipFace;
 export = AipFace;
